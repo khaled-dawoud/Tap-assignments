17d28d75a6da585d588647a13e1a876dc36df157
khaled <khalddawd967@gmail.com>
2023-09-24 17:43:06 +0300

    js-assigment added

---
+++ javascript-assignmen.js
@@ -0,0 +1,18 @@
+function twoSum(nums, target) { 
+    let result = [];
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i+1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) {
+                result.push(i);
+                result.push(j);
+            }
+        }
+    }
+    return result;
+  }
+  
+  const nums = [2, 7, 11, 15];
+  const target = 9;
+  const result = twoSum(nums, target);
+  console.log(result); 
+  
